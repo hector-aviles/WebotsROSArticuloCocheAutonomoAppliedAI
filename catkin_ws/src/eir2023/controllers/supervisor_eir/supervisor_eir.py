d8483a0227716067000d383bcf59232c129a7e73
Tu Nombre <you@example.com>
2023-06-16 19:41:00 -0600

    cambios

--- catkin_ws/src/eir2023/controllers/supervisor_eir/supervisor_eir.py
+++ catkin_ws/src/eir2023/controllers/supervisor_eir/supervisor_eir.py
@@ -16,9 +16,19 @@ def callback_start(msg):
 def main():
     global start
     print('Starting Controller Supervisor...')
+    
     cars = [robot.getFromDef('obstacle_north'), robot.getFromDef('obstacle_north_west'),
             robot.getFromDef('obstacle_west' ), robot.getFromDef('obstacle_south_west')]
+<<<<<<< HEAD
+    
+  
+    bmw  = robot.getFromDef('BMW_X5')  
+    
+    linear_velocity_field = cars.getField("translation")
+              
+=======
     bmw  = robot.getFromDef('BMW_X5')            
+>>>>>>> 4143c6923d6744f01fc2df6b1a92bfdcdda95d93
     start = False
     rospy.init_node("supervisor_node")
     rospy.Subscriber("/start", Empty, callback_start)
@@ -36,7 +46,14 @@ def main():
         msg_bmw_pose.x = bmw_position[0]
         msg_bmw_pose.y = bmw_position[1]
         msg_bmw_pose.theta = math.atan2(bmw_orientation[3], bmw_orientation[0])
-        pub_bmw_pose.publish(msg_bmw_pose)                    
+
+        pub_bmw_pose.publish(msg_bmw_pose)  
+        
+        linear_velocity = linear_velocity_field.getSFVec3f()
+        linear_x, linear_y, linear_z = linear_velocity
+        print("Linear velocity: ", linear_x, linear_y, linear_z)
+                          
+
         loop.sleep()
         
 if __name__ == "__main__":
