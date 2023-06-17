b372e688bb33cde987248b2ac0fe11807830a51f
Mark The Magnificent <marco.negrete@ingenieria.unam.edu>
2023-06-16 20:08:55 -0600

    Modify supervisor to publish obstacle north velocity

--- catkin_ws/src/eir2023/controllers/supervisor_eir/supervisor_eir.py
+++ catkin_ws/src/eir2023/controllers/supervisor_eir/supervisor_eir.py
@@ -4,6 +4,7 @@ import rospy
 import math
 from std_msgs.msg import Empty
 from geometry_msgs.msg import Pose2D
+from geometry_msgs.msg import Twist
 from controller import Supervisor
 
 TIME_STEP = 10
@@ -19,22 +20,28 @@ def main():
     
     cars = [robot.getFromDef('obstacle_north'), robot.getFromDef('obstacle_north_west'),
             robot.getFromDef('obstacle_west' ), robot.getFromDef('obstacle_south_west')]
-<<<<<<< HEAD
-    
-  
     bmw  = robot.getFromDef('BMW_X5')  
-    
-    linear_velocity_field = cars.getField("translation")
-              
-=======
-    bmw  = robot.getFromDef('BMW_X5')            
->>>>>>> 4143c6923d6744f01fc2df6b1a92bfdcdda95d93
+
+    print('breakpoint 1...')
+    #linear_velocity_North = cars[0].getField("translation")
+    print('breakpoint 1.5 ...')
     start = False
     rospy.init_node("supervisor_node")
     rospy.Subscriber("/start", Empty, callback_start)
-    pub_bmw_pose = rospy.Publisher('/current_pose', Pose2D, queue_size=10)    
+    print('breakpoint 2...')
+    pub_bmw_pose  = rospy.Publisher('/current_pose', Pose2D, queue_size=10)
+    pub_obs_N_vel = rospy.Publisher('/obstacle_north_vel', Twist, queue_size=10)
+    pub_obs_NW_vel = rospy.Publisher('/obstacle_north_west_vel', Twist, queue_size=10)
+    pub_obs_W_vel = rospy.Publisher('/obstacle_west_vel', Twist, queue_size=10)
+    pub_obs_SW_vel = rospy.Publisher('/obstacle_south_west_vel', Twist, queue_size=10)
+    msg_N_vel = Twist()
+    msg_NW_vel = Twist()
+    msg_W_vel = Twist()
+    msg_SW_vel = Twist()
+    
     msg_bmw_pose = Pose2D()
     loop = rospy.Rate(1000/TIME_STEP)
+    print('breakpoint 3...')
     while robot.step(TIME_STEP) != -1 and not rospy.is_shutdown():
         if start:
             start = False
@@ -46,12 +53,13 @@ def main():
         msg_bmw_pose.x = bmw_position[0]
         msg_bmw_pose.y = bmw_position[1]
         msg_bmw_pose.theta = math.atan2(bmw_orientation[3], bmw_orientation[0])
-
-        pub_bmw_pose.publish(msg_bmw_pose)  
         
-        linear_velocity = linear_velocity_field.getSFVec3f()
-        linear_x, linear_y, linear_z = linear_velocity
-        print("Linear velocity: ", linear_x, linear_y, linear_z)
+        pub_bmw_pose.publish(msg_bmw_pose)  
+
+        x,y,z,R,P,Y = cars[0].getVelocity()
+        msg_N_vel.linear.x, msg_N_vel.linear.y, msg_N_vel.linear.z = x,y,z
+        msg_N_vel.angular.x, msg_N_vel.angular.y, msg_N_vel.angular.z = R,P,Y
+        pub_obs_N_vel.publish(msg_N_vel)
                           
 
         loop.sleep()
